7838cce41bc7f19c8c4a96be473dcac1abef6034
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-09-08 17:59:14 +0000

    Remove need for vault provider

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = "~> 1.0"
   required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "~> 4.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
